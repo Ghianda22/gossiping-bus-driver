f8971a997c5f3d33676ab1280878e34244d98712
Ghianda22 <valelore98@gmail.com>
2023-05-31 12:11:26 +0200

    2nd test: if two bus drivers are at the same stop they exchange gossips
    When bus drivers exchange gossips, they include each other in their gossip network, to track who already gossiped

--- src/BusDriver.ts
+++ src/BusDriver.ts
@@ -1,17 +1,42 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const MINUTES_OF_WORK_PER_DAY: number = 480;
+
 export default class BusDriver {
     #_dayRoute: number[] = [];
     #_route: number[] = [];
+    #_gossipNet: Set<string> = new Set();
+    #_id: string = uuidv4();
+
 
     constructor(route: number[]) {
         this.#_route = route;
         // initialize dayRoute
-        for (let i = 0; i < 480; i++) {
+        for (let i = 0; i < MINUTES_OF_WORK_PER_DAY; i++) {
             this.#_dayRoute.push(route[i%route.length]);
         }
+    }
+
+
+    get gossipNet(): Set<string> {
+        return this.#_gossipNet;
+    }
+
+    get id(): string {
+        return this.#_id;
     }
 
     get dayRoute(): number[] {
         return this.#_dayRoute;
     }
 
+
+    gossipWith(busDriver1: BusDriver) {
+        for (let stop = 0; stop < MINUTES_OF_WORK_PER_DAY; stop++) {
+            if(busDriver1.dayRoute[stop] == this.dayRoute[stop]){
+                busDriver1.gossipNet.add(this.id);
+                this.gossipNet.add(busDriver1.id);
+            }
+        }
+    }
 }
--- tests/BusDriver.test.ts
+++ tests/BusDriver.test.ts
@@ -2,13 +2,30 @@ import BusDriver from "../src/BusDriver";
 
 test('Given a route, the total day of work should be that route repeated to reach 480 minutes of driving', () => {
     //given
-
     let busDriver: BusDriver = new BusDriver([3,1,2,3]);
 
     //when
 
     //then
     expect(busDriver.dayRoute.length).toEqual(480);
+})
+
+test('Given two day routes, the first time the stops coincide then should be recorded as a gossip exchange for both the bus drivers', () => {
+    //given
+    const busDriver0: BusDriver = new BusDriver([3,1,2,3]);
+    const busDriver1: BusDriver = new BusDriver([3,2,3,1]);
+    const expectedGossipNet0: Set<string> = new Set<string>([busDriver1.id]);
+    const expectedGossipNet1: Set<string> = new Set<string>([busDriver0.id]);
+
+    //when
+    busDriver0.gossipWith(busDriver1);
+    const actualGossipNet0: Set<string> = busDriver0.gossipNet;
+    const actualGossipNet1: Set<string> = busDriver1.gossipNet;
+
+
+    //then
+    expect(actualGossipNet0).toEqual(expectedGossipNet0)
+    expect(actualGossipNet1).toEqual(expectedGossipNet1)
 })
 
 
